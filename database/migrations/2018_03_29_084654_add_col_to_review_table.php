ce83c9fd8509ec1d79bbc5f27821c6dbdc84490e
TrinhCDanh <trinhdanh04@gmail.com>
2018-03-29 15:49:26 +0700

    create migration

--- database/migrations/2018_03_29_084654_add_col_to_review_table.php
+++ database/migrations/2018_03_29_084654_add_col_to_review_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateReviewsTable extends Migration
+class AddColToReviewTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateReviewsTable extends Migration
      */
     public function up()
     {
-        Schema::create('reviews', function (Blueprint $table) {
-            $table->increments('id');
+        Schema::table('reviews', function (Blueprint $table) {
             $table->integer('products_id')->unsigned()->index();
             $table->foreign('products_id')->references('id')->on('products')->onDelete('cascade');
             $table->string('customer');
             $table->text('review');
             $table->integer('star');
-            $table->timestamps();
         });
     }
 
@@ -31,6 +29,8 @@ class CreateReviewsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('reviews');
+        Schema::table('reviews', function (Blueprint $table) {
+            //
+        });
     }
 }
